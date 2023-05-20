b25cf5913b8ada23d166efb709001c5fc7b34a8f
Pranav Patil <121039421+pranavpatil15198@users.noreply.github.com>
2023-05-20 20:52:58 +0530

    u

--- src/main/java/com/driver/Student.java
+++ src/main/java/com/driver/Student.java
@@ -1,40 +1,23 @@
 package com.driver;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 
 public class Student {
+
     private String name;
     private int age;
     private double averageScore;
 
-    public Student() {
+    public Student(){
+
+    }
+
+    public Student(String name, int age, double averageScore) {
         this.name = name;
         this.age = age;
         this.averageScore = averageScore;
     }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    public double getAverageScore() {
-        return averageScore;
-    }
-
-    public void setAverageScore(double averageScore) {
-        this.averageScore = averageScore;
-    }
-
-
 }
-
