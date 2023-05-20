8ad7caba3c70da64ba873e37e3519fcbb8c7c5f5
Pranav Patil <121039421+pranavpatil15198@users.noreply.github.com>
2023-05-20 21:53:35 +0530

    Update StudentService.java

--- src/main/java/com/driver/StudentService.java
+++ src/main/java/com/driver/StudentService.java
@@ -3,40 +3,45 @@ package com.driver;
 import java.util.List;
 
 public class StudentService {
+
     StudentRepository studentRepository=new StudentRepository();
     public void addStudent(Student student) {
-        studentRepository.addStudent(student);
+        studentRepository.addstudent(student);
     }
+    
 
     public void addTeacher(Teacher teacher) {
-        studentRepository.addTeacher(teacher);
+        studentRepository.addteacher(teacher);
     }
 
-    public void addStudentTeacherPair(String student, String teacher) {
-        studentRepository.addStudentTeacherPair(student,teacher);
+    public void addStudentTeacher(String student, String teacher) {
+        studentRepository.addstudentTeacher(student,teacher);
     }
 
-    public Student getStudentByName(String name) {
-        return studentRepository.getStudentByName(name);
+    public Student studentByName(String name) {
+        Student student=studentRepository.studentByName(name);
+        return student;
     }
 
-    public Teacher getTeacherByName(String name) {
-        return studentRepository.getTeacherByName(name);
+    public Teacher teacherByName(String name) {
+        Teacher teacher=studentRepository.teacherByName(name);
+        return teacher;
     }
 
-    public List<String> getStudentsByTeacherName(String teacher) {
-        return studentRepository.getStudentsByTeacherName(teacher);
+    public List<String> getStudentByTeacherName(String teacher) {
+        return studentRepository.getStudentByTeacherName(teacher);
+
     }
 
-    public List<String> getAllStudents() {
-        return studentRepository.getAllStudents();
+    public List<String> getAllStudent() {
+        return studentRepository.getAllStudent();
     }
 
     public void deleteTeacherByName(String teacher) {
         studentRepository.deleteTeacherByName(teacher);
     }
 
-    public void deleteAllTeachers() {
-        studentRepository.deleteAllTeachers();
+    public void deleteAllTeacher() {
+     studentRepository.deleteAllTeacher();
     }
 }
